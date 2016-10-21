77fc494698f5d6d5c2d0e38ae7396ec43d90fd15
Selby Kendrick <selby.kendrick@gmail.com>
2016-10-21 15:33:30 -0700

    Refactor identifier renaming

--- minifier/html/strategies/optimize-css-identifiers.js
+++ minifier/html/strategies/optimize-css-identifiers.js
@@ -1,59 +1,94 @@
 'use strict';
 
-function nextName(currentClassName) {
-    currentClassName = currentClassName || '@';
-    let code = currentClassName.charCodeAt(currentClassName.length - 1);
+/**
+ * Next shortest unique string in format /([A-Z]|[a-z])+/
+ * @param  {String} last used to generate next shortest string
+ * @return {String}                  next shortest unique string
+ */
+function nextName(last) {
+    last = last || '@';
+    let code = last.charCodeAt(last.length - 1);
     if (code === 122) {
-        currentClassName += 'A';
+        last += 'A';
     } else {
         if (code === 90) {
             code = 96;
         }
-        currentClassName = currentClassName.replace(/.$/, '') + String.fromCharCode(code + 1);
+        last = last.replace(/.$/, '') + String.fromCharCode(code + 1);
     }
-    return currentClassName;
+    return last;
 }
 
 module.exports = function() {
     return function parse(tree) {
+        // Map original strings to replacement strings
+        let idMap = {};
         let classNameMap = {};
 
+        // Next shortest string
+        let currentIdRename = nextName();
         let currentClassRename = nextName();
 
+        // For each tree node that has an id attribute:
+        //  Create mapping if one doesn't already exist
+        //  in idMap and then replace the id with it
         tree.match({
-            tag: 'body'
+            attrs: {
+                id: /.+/
+            }
         }, function(node) {
-            node.content.forEach(element => {
-                if (typeof element === 'object') {
-                    if (element.attrs.class) {
-                        let classes = element.attrs.class.split(/\s+/g);
-                        let eleClassAttr = '';
-                        for (let i = 0; i < classes.length; ++i) {
-                            if (classNameMap[classes[i]]) {
-                                eleClassAttr = eleClassAttr === '' ? classNameMap[classes[i]] : eleClassAttr + ' ' + classNameMap[classes[i]];
-                            } else {
-                                classNameMap[classes[i]] = currentClassRename;
-                                eleClassAttr = eleClassAttr === '' ? currentClassRename : eleClassAttr + ' ' + currentClassRename;
-                                currentClassRename = nextName(currentClassRename);
-                            }
-                        }
-                        element.attrs.class = eleClassAttr;
-                    }
-                }
-            });
+            if (idMap[node.attrs.id]) {
+                node.attrs.id = idMap[node.attrs.id];
+            } else {
+                idMap[node.attrs.id] = currentIdRename;
+                currentIdRename = nextName(currentIdRename);
+            }
             return node;
         });
 
+        // For each tree node that has class attribute:
+        //  Create mapping if one doesn't already exist
+        //  in classNameMap and then replace the class name(s)
+        //  with it
+        tree.match({
+            attrs: {
+                class: /.+/
+            }
+        }, function(node) {
+            // Break class attr into its classes
+            let classes = node.attrs.class.split(/\s+/g);
+            // Build new classAttr
+            let classAttr = '';
+            for (let i = 0; i < classes.length; ++i) {
+                if (classNameMap[classes[i]]) {
+                    classAttr = classAttr === '' ? classNameMap[classes[i]] : classAttr + ' ' + classNameMap[classes[i]];
+                } else {
+                    classNameMap[classes[i]] = currentClassRename;
+                    classAttr = classAttr === '' ? currentClassRename : classAttr + ' ' + currentClassRename;
+                    currentClassRename = nextName(currentClassRename);
+                }
+            }
+            node.attrs.class = classAttr;
+            return node;
+        });
+
+        // Replace ids and classnames in style elements
         tree.match({
             tag: 'style'
         }, function(node) {
-            let classNames = Object.keys(classNameMap);
-            for (let i = 0; i < classNames.length; ++i) {
-                node.content = node.content.map(styles => { // jshint ignore:line
-                    const regex = new RegExp('\.' + classNames[i]);
-                    return styles.replace(regex, '.' + classNameMap[classNames[i]]);
-                });
-            }
+            node.content = node.content.map(styles => { // jshint ignore:line
+                // Replace ids
+                for (let id in idMap) {
+                    const regex = new RegExp('#' + id);
+                    styles = styles.replace(regex, '#' + idMap[id]);
+                }
+                // Replace class names
+                for (let className in classNameMap) {
+                    const regex = new RegExp('\.' + className);
+                    styles = styles.replace(regex, '.' + classNameMap[className]);
+                }
+                return styles;
+            });
             return node;
         });
 
