6b74c9f357bc73360658f8f89e323ed6b2de2391
Selby Kendrick <selby.kendrick@gmail.com>
2016-10-21 13:31:04 -0700

    organize minifier tests better

--- test/minifier.js
+++ test/minifier.js
@@ -15,7 +15,7 @@ describe('minifier', () => {
         done();
     });
 
-    it('works', (done) => {
+    it('removes whitespace', (done) => {
         const inputHtml = `
 <component>
   <title>Super Title</title>
@@ -25,30 +25,6 @@ describe('minifier', () => {
 
         const expectedOutput = `
 <component><title>Super Title</title><text>Awesome Text</text></component>
-`;
-
-        minifier(inputHtml)
-            .then(minifiedHtml => {
-                expect(minifiedHtml).to.equal(expectedOutput);
-                done();
-            })
-            .catch(done);
-    });
-
-    it('combines style tags and places them in <head>', (done) => {
-        const inputHtml = `
-<html>
-<head>
-</head>
-<body>
-  <style>.red {color: red;}</style>
-  <style>.white {color: white;}</style>
-</body>
-</html>
-`;
-
-        const expectedOutput = `
-<html><head><style>.red {color: red;}.white {color: white;}</style></head><body></body></html>
 `;
 
         minifier(inputHtml)
@@ -100,66 +76,72 @@ describe('minifier', () => {
             .catch(done);
     });
 
-    it('optimize ids and class names', (done) => {
-        const inputHtml = `
+    describe('strategy optimizeClassNames', () => {
+        it('optimizes ids and class names', (done) => {
+            const inputHtml = `
 <html><head><style>body {background-color: #FFF;}.red {color: red;}.white {color: white;}.lead {font-weight: bold;}</style></head><body><h1 class="red">RED AT YOUR SERVICE</h1><p class="white lead">But this is white.</p></body></html>
 `;
 
-        const expectedOutput = `
+            const expectedOutput = `
 <html><head><style>body {background-color: #FFF;}.A {color: red;}.B {color: white;}.C {font-weight: bold;}</style></head><body><h1 class="A">RED AT YOUR SERVICE</h1><p class="B C">But this is white.</p></body></html>
 `;
 
-        const opts = {
-            optimizeClassNames: true
-        };
+            const opts = {
+                optimizeClassNames: true
+            };
 
-        minifier(inputHtml, opts)
-            .then(minifiedHtml => {
-                expect(minifiedHtml).to.equal(expectedOutput);
-                done();
-            })
-            .catch(done);
+            minifier(inputHtml, opts)
+                .then(minifiedHtml => {
+                    expect(minifiedHtml).to.equal(expectedOutput);
+                    done();
+                })
+                .catch(done);
+        });
     });
 
-    it('remove unused classes', (done) => {
-        const inputHtml = `
+    describe('strategy removeUnusedClasses', () => {
+        it('removes unused CSS classes from styles and elements', (done) => {
+            const inputHtml = `
 <html><head><style>.red {color: red;} .lead {font-weight: 900;}</style></head><body><p class="white lead">But this is white and bold.</p><p class="white">But this is just white.</p></body></html>
 `;
 
-        const expectedOutput = `
+            const expectedOutput = `
 <html><head><style>.lead{font-weight:900;}</style></head><body><p class="lead">But this is white and bold.</p><p>But this is just white.</p></body></html>
 `;
 
-        const opts = {
-            removeUnusedClasses: true
-        };
+            const opts = {
+                removeUnusedClasses: true
+            };
 
-        minifier(inputHtml, opts)
-            .then(minifiedHtml => {
-                expect(minifiedHtml).to.equal(expectedOutput);
-                done();
-            })
-            .catch(done);
+            minifier(inputHtml, opts)
+                .then(minifiedHtml => {
+                    expect(minifiedHtml).to.equal(expectedOutput);
+                    done();
+                })
+                .catch(done);
+        });
     });
 
-    it('strategy useShortHex that replaces HEX with its shorthand (#FFFFFF => #FFF)', (done) => {
-        const inputHtml = `
-<html><head><style>.red {color: #ff0000;}.white {color: #ffffff;}.tan{color:#ededed}</style></head><body></body></html>
-`;
+    describe('strategy useShortHex', () => {
+        it('replaces HEX with its shorthand (#FFFFFF => #FFF)', (done) => {
+            const inputHtml = `
+  <html><head><style>.red {color: #ff0000;}.white {color: #ffffff;}.tan{color:#ededed}</style></head><body></body></html>
+  `;
 
-        const expectedOutput = `
-<html><head><style>.red {color: #f00;}.white {color: #fff;}.tan{color:#ededed}</style></head><body></body></html>
-`;
+            const expectedOutput = `
+  <html><head><style>.red {color: #f00;}.white {color: #fff;}.tan{color:#ededed}</style></head><body></body></html>
+  `;
 
-        const opts = {
-            useShortHex: true
-        };
+            const opts = {
+                useShortHex: true
+            };
 
-        minifier(inputHtml, opts)
-            .then(minifiedHtml => {
-                expect(minifiedHtml).to.equal(expectedOutput);
-                done();
-            })
-            .catch(done);
+            minifier(inputHtml, opts)
+                .then(minifiedHtml => {
+                    expect(minifiedHtml).to.equal(expectedOutput);
+                    done();
+                })
+                .catch(done);
+        });
     });
 });
